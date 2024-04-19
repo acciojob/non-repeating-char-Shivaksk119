261a81cadb4d6ed96af54ed865ea75e85a2cc82f
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-04-20 02:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let charCount = {};
+	for(let i=0;i<str.length;i++) {
+		if(charCount[str.charAt(i)]==undefined) {
+			charCount[str.charAt(i)]] = 1;
+		}
+		else {
+			charCount[str.charAt(i)]] += 1;
+		}
+	}
+
+	for (c in charCount) {
+		if(c==1) {
+			return c;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
